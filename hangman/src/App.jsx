c17ef55162684f0442ffc8362410a74f65ae45ba
Lord Stoker <tpc00@iesemilidarder.com>
2025-01-13 11:11:37 +0100

    gameStatus

--- hangman/src/App.jsx
+++ hangman/src/App.jsx
@@ -1,11 +1,19 @@
 import {useState} from 'react'
 import {languages} from './languages.js'
 import {clsx} from 'clsx';
+import Confetti from 'react-confetti';
 
 export default function App() {
 
-  const[currentWord, setCurrentWord] = useState('react'.toUpperCase());
+  //STATE VARIABLES
+  const[currentWord, setCurrentWord] = useState('recuperacion'.toUpperCase());
   const[guessedLetters, setGuessedLetters] = useState([]);
+  // const[wrongAttempts, setWrongAttempts] = useState(0);
+  //DERIVED VARIABLES
+  const wrongAttempts = guessedLetters.filter(letter => !currentWord.includes(letter)).length;
+  const rightAttempts = guessedLetters.filter(letter => currentWord.includes(letter)).length;
+  const isGameWon = currentWord.split('').every(letter => guessedLetters.includes(letter));
+  const isGameLost = wrongAttempts >= languages.length -1 || rightAttempts === currentWord.length;
 
   //ALFABETO
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -19,8 +27,6 @@ export default function App() {
       wrong: isWrong
       }
     );
-
-
       // const backGroundColor = isGuessed ? {backgroundColor: 'green'} : {backgroundColor: 'red'};
     return(
         <button 
@@ -31,27 +37,35 @@ export default function App() {
         </button>
       )
   });
-//AÑADIR LETRAS SI NO SE REPITEN
+
+
+//AÑADIR LETRAS SI NO SE REPITEN Y RESTA 1 VIDA POR INTENTO FALLIDO
   function addGuessedLetter(letter) {
     setGuessedLetters(prevGuessedLetters =>
       prevGuessedLetters.includes(letter) ? prevGuessedLetters :
       [...prevGuessedLetters, letter]);
+    // if(!currentWord.includes(letter)) {
+    //   setWrongAttempts(prevWrongAttempts => prevWrongAttempts + 1);
+    // }
   }
 
   //PALABRA A ADIVINAR
   const wordLetters = currentWord.split('').map((letter, index) => {
-    return(
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    return(      
       <span className='letter' key={index}>
-        {letter.toUpperCase()}
+        {isCorrect ? letter.toUpperCase() : ''}
       </span>
     )
   }
-  );
+);
   //CHIPS DE LENGUAJES (VIDAS)
-  const languagesChips = languages.map(language => {
+  const languagesChips = languages.map((language, index) => {
+    const className = clsx('chip', index < wrongAttempts && 'lost'); 
     return(
       <span 
-        className='chip'
+        className={className}
         key={language.name}
         style={{backgroundColor: language.backgroundColor, color: language.color}}
       >
@@ -66,10 +80,21 @@ export default function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className='game-Status'>
-        <h2>You win!</h2>
-        <p>Well done 🎉</p>
-      </section>
+      {
+        isGameWon &&
+        <section className='game-status'>
+          <Confetti/>
+          <h2>You win!</h2>
+          <p>Well done 🎉</p>
+        </section>
+        }
+      {
+          isGameLost &&
+          <section className='youLose'>
+            <h2>Game over</h2>
+            <p>You lose! Better start learning Assembly 😭</p>
+          </section>
+        }
       <section className='language-chips'>
         {languagesChips}
       </section>
@@ -79,7 +104,7 @@ export default function App() {
       <section className='keyboard'>
         {keyboard}
       </section>
-      <button className='new-Game'>New Game</button>
+      {isGameWon && <button className='new-game'>New Game</button>}
 
 
       {/* <section className='youLose'>
